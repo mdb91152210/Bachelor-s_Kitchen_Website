693c4686691a644cd5521ffd158b73eb9c58b6f8
samples testing <mohamedbilal9115@gmail.com>
2026-07-06 12:47:27 +0530

    first view

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Config project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from homeapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
